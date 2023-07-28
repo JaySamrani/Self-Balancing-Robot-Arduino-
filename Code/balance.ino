d310a71ddd85fa7aced2fd3474f8ef2c5603a2ed
JaySamrani <127646718+JaySamrani@users.noreply.github.com>
2023-07-28 17:59:20 +0530

    Add files via upload

---
+++ Code/balance.ino
@@ -0,0 +1,130 @@
+#include <Wire.h>
+////////////////VARIABLE DEFINATION///////////////
+int rmotor1 = 6;
+int rmotor2 = 7;
+
+int mspeed = 10;
+int turnspeed=50;
+int16_t Acc_rawX, Acc_rawY, Acc_rawZ,Gyr_rawX, Gyr_rawY, Gyr_rawZ;
+float Acceleration_angle[2];
+float Gyro_angle[2];
+float Total_angle[2];
+float elapsedTime, time, timePrev;
+int i;
+float rad_to_deg = 180/3.141592654;
+float PID, pwmLeft, pwmRight, error, previous_error;
+float pid_p=0;
+float pid_i=0;
+float pid_d=0;
+////////////////////////PID CONSTANST/////////////////////
+float kp=27;
+float ki=30;
+float kd=0.8;
+float desired_angle = 0;//////////////TARGET ANGLE/////////////
+void setup() 
+{
+  Wire.begin(); /////////////TO BEGIN I2C COMMUNICATIONS///////////////
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  ////////////////PIN MODE DEFINATIONS//////////////////////
+  pinMode(rmotor1,OUTPUT);
+
+  pinMode(rmotor2,OUTPUT);
+
+  Serial.begin(9600);
+  time = millis(); ///////////////STARTS COUNTING TIME IN MILLISECONDS/////////////
+}
+void loop() 
+{
+
+    timePrev = time;  
+    time = millis();  
+    elapsedTime = (time - timePrev) / 1000; 
+    Wire.beginTransmission(0x68);
+    Wire.write(0x3B); 
+    Wire.endTransmission(false);
+    Wire.requestFrom(0x68,6,true);
+    ////////////////////PULLING RAW ACCELEROMETER DATA FROM IMU///////////////// 
+    Acc_rawX=Wire.read()<<8|Wire.read(); 
+    Acc_rawY=Wire.read()<<8|Wire.read();
+    Acc_rawZ=Wire.read()<<8|Wire.read();
+     
+    /////////////////////CONVERTING RAW DATA TO ANGLES/////////////////////
+    Acceleration_angle[0] = atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
+    Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
+    Wire.beginTransmission(0x68);
+    Wire.write(0x43); 
+    Wire.endTransmission(false);
+    Wire.requestFrom(0x68,4,true); 
+    
+    //////////////////PULLING RAW GYRO DATA FROM IMU/////////////////////////
+    Gyr_rawX=Wire.read()<<8|Wire.read(); 
+    Gyr_rawY=Wire.read()<<8|Wire.read(); 
+    
+    ////////////////////CONVERTING RAW DATA TO ANGLES///////////////////////
+    Gyro_angle[0] = Gyr_rawX/131.0; 
+    Gyro_angle[1] = Gyr_rawY/131.0;
+    
+    //////////////////////////////COMBINING BOTH ANGLES USING COMPLIMENTARY FILTER////////////////////////
+    Total_angle[0] = 0.98 *(Total_angle[0] + Gyro_angle[0]*elapsedTime) + 0.02*Acceleration_angle[0];
+    Total_angle[1] = 0.98 *(Total_angle[1] + Gyro_angle[1]*elapsedTime) + 0.02*Acceleration_angle[1];
+    ////TOTAL_ANGLE[1] IS THE ROll ANGLE WHICH WE NEED////////////
+    
+    error = Total_angle[1] - desired_angle; /////////////////ERROR CALCULATION////////////////////
+    ///////////////////////PROPORTIONAL ERROR//////////////
+    pid_p = kp*error;
+    ///////////////////////INTERGRAL ERROR/////////////////
+    pid_i = pid_i+(ki*error*elapsedTime);  
+    ///////////////////////DIFFERENTIAL ERROR//////////////
+    pid_d = kd*((error - previous_error)/elapsedTime);
+    ///////////////////////TOTAL PID VALUE/////////////////
+    PID = pid_p +pid_d; 
+    ///////////////////////UPDATING THE ERROR VALUE////////
+    previous_error = error;
+    
+    // Serial.println(PID);                     //////////UNCOMMENT FOR DDEBUGGING//////////////
+    // delay(60);                               //////////UNCOMMENT FOR DDEBUGGING//////////////
+    // Serial.println(Total_angle[0]);          //////////UNCOMMENT FOR DDEBUGGING//////////////
+    
+    /////////////////CONVERTING PID VALUES TO ABSOLUTE VALUES//////////////////////////////////
+    mspeed = abs(PID);
+    //Serial.println(mspeed);                  //////////UNCOMMENT FOR DDEBUGGING//////////////
+    
+    if(Total_angle[1]<0)
+      {
+       anti();
+      }
+    if(Total_angle[1]>0)
+      {
+       clockw();
+      }
+    if(Total_angle[1]>45)
+    halt();
+    if(Total_angle[1]<-45)
+    halt();
+    
+}
+//////////////MOVEMENT FUNCTION///////////////////
+void clockw()
+{
+  analogWrite(rmotor1,mspeed);
+  analogWrite(rmotor2,0);
+
+}
+void anti()
+{
+
+  analogWrite(rmotor2,mspeed);
+  analogWrite(rmotor1,0);
+
+}
+void halt()
+{
+  
+  analogWrite(rmotor1,0);
+  analogWrite(rmotor2,0);
+
+  
+}
